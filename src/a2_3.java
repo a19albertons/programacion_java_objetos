add980dba4757b6aa69a502cd4e0bb77db9609ae
a19albertons <a19albertons@iessanclemente.net>
2025-05-13 10:45:00 +0200

    Objetos java 12/05/2025 Patrones de diseño

---
+++ src/a2_3.java
@@ -0,0 +1,43 @@
+import java.math.BigDecimal;
+
+class Articulo{
+    String descripcion;
+    BigDecimal precio;
+    Descuento descuento;
+
+    public Articulo(String descripcion, BigDecimal precio, Descuento descuento) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.descuento = descuento;
+    }
+
+    public BigDecimal pvp(){
+        return precio.subtract(descuento.aplicarDescuento(precio));
+    }
+    public void setDescuento(Descuento descuento){
+        this.descuento=descuento;
+    }
+}
+interface Descuento {
+    BigDecimal aplicarDescuento(BigDecimal cantidad);
+}
+class DescuentoNavidad implements Descuento {
+    @Override
+    public BigDecimal aplicarDescuento(BigDecimal cantidad) {
+        return cantidad.multiply(BigDecimal.valueOf(0.5));
+    }
+}
+class DescuentoVerano implements Descuento{
+    @Override
+    public BigDecimal aplicarDescuento(BigDecimal cantidad) {
+        return cantidad.multiply(BigDecimal.valueOf(0.3));
+    }
+}
+public class a2_3 {
+    public static void main(String[] args) {
+        Articulo a1= new Articulo("Jamón pachuly", new BigDecimal("100"), new DescuentoNavidad());
+        System.out.println(a1.pvp());
+        a1.setDescuento(new DescuentoVerano());
+        System.out.println(a1.pvp());
+    }
+}
